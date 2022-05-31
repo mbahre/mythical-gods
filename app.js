cd02a38d9d8557bcd9f2d1a4db2a4851f70685bf
michael <michaelbahreini@gmail.com>
2022-05-31 16:37:14 -0500

    implemented rate-limiter

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const mgRouter = require(`${__dirname}/routes/mgRoute`);
 
 const app = express();
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests, Please try again later.",
+});
+
+app.use("/api", limiter);
 
 app.use(morgan("dev"));
 
