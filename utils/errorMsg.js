5a055fa09c100c7c2cd239623b644f5b35153785
michael <michaelbahreini@gmail.com>
2022-06-01 12:59:57 -0500

    small fix

--- utils/errorMsg.js
+++ utils/errorMsg.js
@@ -2,8 +2,6 @@ function errorMsg(err, req, res, next) {
   if (err.message === "This page does not exist") {
     res.status(404).json({ status: "fail", message: err.message });
   }
-
-  next();
 }
 
 module.exports = errorMsg;
