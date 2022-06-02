ce3e8fe050a5e4245a9422200dc9788efbc1d4ab
michael <michaelbahreini@gmail.com>
2022-06-02 13:10:11 -0500

    changed error export

--- utils/errorMsg.js
+++ utils/errorMsg.js
@@ -1,7 +1,5 @@
-function errorMsg(err, req, res, next) {
+module.exports = function (err, req, res, next) {
   if (err.message === "This page does not exist") {
     res.status(404).json({ status: "fail", message: err.message });
   }
-}
-
-module.exports = errorMsg;
+};
